fc061073b3dae45229b2a6938ae865619c2b2ef4
dojt <dotheis.work@gmail.com>
2020-04-05 10:02:12 +0300

    new

---
+++ bin/install_QuEST.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo cd to QuEST
+cd $HOME/my.local/share/QuEST                   || exit 1
+echo git pull
+git pull                                        || exit 1
+
+find . -type f -exec sed -i s/'-mavx'//g {} \;  || exit 1
+rm -rf build
+mkdir build
+cd build                                        || exit 1
+cmake ..                                        || exit 1
+make                                            || exit 1
+./demo                                          || exit 1
+
+cd ../../..                                     || exit 1
+cd include                                      || exit 1
+ln -fs ../share/QuEST/QuEST/include/*.h .       || exit 1
+cd ../lib                                       || exit 1
+ln -fs ../share/QuEST/build/QuEST/libQuEST.so . || exit 1
+
+
+echo Done.
