478f340004831236e953fada2c4438a79944ba1b
dojt <dotheis.work@gmail.com>
2020-04-27 19:17:07 +0300

    fixed /include/Unity folder

--- bin/install_Unity.sh
+++ bin/install_Unity.sh
@@ -4,11 +4,11 @@ cd $HOME/my.local/share/Unity                 || exit 1
 echo git pull
 git pull                                      || exit 1
 
-mkdir $HOME/my.local/include/Unity            2>/dev/null
-rm $HOME/my.local/include/Unity/*.h           2>/dev/null
+# mkdir $HOME/my.local/include/            2>/dev/null
+# rm $HOME/my.local/include/unity*.h           2>/dev/null
 rm $HOME/my.local/lib/libUnity.a              2>/dev/null
-cd $HOME/my.local/include/Unity               || exit 1
-ln -s `find ../../share/Unity/ -name \*.h | grep /src/ | grep -v /test/ | grep -v /examples/` .  || exit 1
+cd $HOME/my.local/include                     || exit 1
+ln -fs `find ../share/Unity/ -name \*.h | grep /src/ | grep -v /test/ | grep -v /examples/` .  || exit 1
 
 cd $HOME/my.local/share/Unity                 || exit 1
 
@@ -17,7 +17,7 @@ do
     pushd $d                                  || exit 1
     for cf in \*.c ;
     do
-	gcc -I$HOME/my.local/include/Unity/ -DUNITY_INCLUDE_DOUBLE -c $cf     || exit 1
+	gcc -I$HOME/my.local/include/ -DUNITY_INCLUDE_DOUBLE -c $cf     || exit 1
     done
     popd
 done;
